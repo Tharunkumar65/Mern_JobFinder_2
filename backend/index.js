266cea45e02ffb9b39011a33407c6945774b268d
Tharun <dugginenitharunkumar25@gmail.com>
2024-08-11 00:03:32 +0530

    change url in backend

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 const corsOptions = {
-    origin:'http://localhost:5173',
+    origin:'https://mern-jobfinder--delta.vercel.app/',
     methods: ['GET', 'POST', 'PUT', 'DELETE'], // Ensure all necessary methods are allowed
     credentials:true
 }
